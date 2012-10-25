e2ebcea1d5b54b1f1bd50d9cdb8b826b2d152c83
Justin R. Miller <incanus@codesorcery.net>
2012-10-25 10:41:18 -0700

    Initial Commit

---
+++ Dissect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dissect
+//
+//  Created by Justin R. Miller on 10/25/12.
+//  Copyright (c) 2012 MapBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
